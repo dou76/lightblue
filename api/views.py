5a282284dd02c45f69ec0de9c0e639a12bf11cd1
zys <zhuys19@mails.tsinghua.edu.cn>
2021-12-25 17:49:15 +0800

    fix bug and reconstruct

--- api/views.py
+++ api/views.py
@@ -1,1 +1,6 @@
 from django.http import HttpResponse
+from django.http.response import JsonResponse
+from django.middleware.csrf import get_token
+
+def get_token_views(request):
+    return JsonResponse ({"csrf_token": get_token(request) or "NOTPROVIDED"})
--- store/middleware.py
+++ store/middleware.py
@@ -1,16 +1,16 @@
 import json
+import re
 from django.http.response import HttpResponse
 from django.contrib.auth import logout
 
 try:
-    from django.utils.deprecation import MiddlewareMixin  # Django 1.10.x
+    from django.utils.deprecation import MiddlewareMixin
 except ImportError:
-    MiddlewareMixin = object  # Django 1.4.x - Django 1.9.x
- 
- 
+    MiddlewareMixin = object
+
 class LoginIntercept(MiddlewareMixin):
     def process_request(self, request):
-        if request.path == "/":
+        if re.search("/store", request.path) is None:
             pass
         elif request.path != '/store/login/' and request.path != '/store/register/':
             if request.session.get('username'):
@@ -20,7 +20,7 @@ class LoginIntercept(MiddlewareMixin):
 
 class ForceLogoutMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if request.path == "/":
+        if not re.search("/store", request.path) is None:
             pass
         elif request.user.is_authenticated and request.user.force_logout_date and \
            request.session['LAST_LOGIN_DATE'] < request.user.force_logout_date.replace(tzinfo = None):
